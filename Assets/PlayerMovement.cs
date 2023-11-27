dc10a90386f3f8e4bffff5bc05892b826decccc9
Adam <adamwaldemarkubis@gmail.com>
2023-11-27 19:36:32 +0100

    Player Animations code

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private int wholeNumber = 0;
+    private Animator animator;
     private float decimalNumber = 21.37f;
     private string text = "Hello, world!";
     private bool boolean = false;
@@ -14,17 +14,39 @@ public class PlayerMovement : MonoBehaviour
     {
         Debug.Log(text);
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         float dirX = Input.GetAxis("Horizontal");
+        bool space = Input.GetKey(KeyCode.Space);
         rb.velocity = new Vector2(dirX*5f, rb.velocity.y);
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
+            animator.SetBool("jumping", true);
         }
-
+        if (dirX > 0f)
+        {
+            animator.SetBool("running", true);
+        }
+        else if (dirX < 0f)
+        {
+            animator.SetBool("running", true);
+        }
+        else
+        {
+            animator.SetBool("running", false);
+        }
+        if (space)
+        {
+            
+        }
+        else
+        {
+            animator.SetBool("jumping", false);
+        }
     }
 }
